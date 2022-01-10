a998a823e910a3791d42b105c9f9cadd6aba187d
Marek Nowakowski <marsznow@gmail.com>
2022-01-10 17:21:32 +0100

    Added other patients with routing /patient/{id}, changed plot colors and layout

--- app.py
+++ app.py
@@ -10,19 +10,22 @@ collector_running = True
 class DataCollectorThread(threading.Thread):
     def run(self):
         while collector_running:
+            add_measurements(1, get_new_data("1"))
             add_measurements(2, get_new_data("2"))
+            add_measurements(3, get_new_data("3"))
+            add_measurements(4, get_new_data("4"))
+            add_measurements(5, get_new_data("5"))
             expire_data(20)
-            print(get_storage())
+            # print(get_storage())
             time.sleep(1)
 
 
 if __name__ == "__main__":
     # Initialization
     init_storage()
-    create_layout()
+
     # Start the collector
     collector = DataCollectorThread()
     collector.start()
     # Start dash app
     app.run_server(debug=True)
-
--- dash_app.py
+++ dash_app.py
@@ -9,21 +9,24 @@ from dash.dependencies import Input, Output
 from storage import get_storage
 
 
+global patient_id
+
+
 def draw_plot():
     if 2 in get_storage():
-        patient_data = get_storage()[2]
+        patient_data = get_storage()[patient_id]
         timestamps = np.array(patient_data["timestamps"])
         values = np.array(patient_data["values"])
     else:
         timestamps = np.array([0])
         values = np.array([[0, 0, 0, 0, 0, 0]])
     fig = go.Figure([
-        go.Scatter(x=timestamps, y=values[:, 0], line=dict(color='firebrick', width=4), name='L0'),
-        go.Scatter(x=timestamps, y=values[:, 1], line=dict(color='blue', width=4), name='L1'),
-        go.Scatter(x=timestamps, y=values[:, 2], line=dict(color='green', width=4), name='L2'),
-        go.Scatter(x=timestamps, y=values[:, 3], line=dict(color='violet', width=4), name='R0'),
-        go.Scatter(x=timestamps, y=values[:, 4], line=dict(color='black', width=4), name='R1'),
-        go.Scatter(x=timestamps, y=values[:, 5], line=dict(color='pink', width=4), name='R2'),
+        go.Scatter(x=timestamps, y=values[:, 0], line=dict(color='darkblue', width=3), name='L0'),
+        go.Scatter(x=timestamps, y=values[:, 1], line=dict(color='blue', width=3), name='L1'),
+        go.Scatter(x=timestamps, y=values[:, 2], line=dict(color='lightblue', width=3), name='L2'),
+        go.Scatter(x=timestamps, y=values[:, 3], line=dict(color='darkgreen', width=3), name='R0'),
+        go.Scatter(x=timestamps, y=values[:, 4], line=dict(color='green', width=3), name='R1'),
+        go.Scatter(x=timestamps, y=values[:, 5], line=dict(color='lime', width=3), name='R2'),
     ])
     return fig
 
@@ -31,9 +34,26 @@ def draw_plot():
 app = dash.Dash()
 
 
-def create_layout():
-    app.layout = html.Div(id='parent', children=[
-        html.H1(id='H1', children='Title',
+app.layout = html.Div([
+    # represents the URL bar, doesn't render anything
+    dcc.Location(id='url', refresh=False),
+
+    html.Div(id='content', children=[
+        html.Div(id='main-graph', children=[
+            html.H1(id='H1', children=f'Patient',
+                    style={'textAlign': 'center', 'marginTop': 40, 'marginBottom': 40}),
+            dcc.Graph(id='the_plot'),
+            dcc.Interval(id='interval', interval=1000, n_intervals=0)
+        ])
+    ])
+])
+
+
+def create_layout(_patient_id):
+    global patient_id
+    patient_id = _patient_id
+    return html.Div(id='main-graph', children=[
+        html.H1(id='H1', children=f'Patient {patient_id}',
                 style={'textAlign': 'center', 'marginTop': 40, 'marginBottom': 40}),
         dcc.Graph(id='the_plot', figure=draw_plot()),
         dcc.Interval(id='interval', interval=1000, n_intervals=0)
@@ -44,3 +64,12 @@ def create_layout():
               [Input(component_id='interval', component_property='n_intervals')])
 def graph_update(n_intervals):
     return draw_plot()
+
+
+@app.callback(Output('content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if len(pathname) == 10 and pathname[0:9] == '/patient/' and int(pathname[9]) in range(1, 6):
+        return create_layout(int(pathname[9]))
+    else:
+        return create_layout(1)
